5286c91294da367e9372e1f11846257a1d5324a7
yassine <lagyassine88@gmail.com>
2020-04-09 19:00:43 +0100

    add link css to master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	
+
+    <!-- CSS
+	================================================== -->
+	<!-- Base + Vendors CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/fonts/font-awesome/css/font-awesome.css">
+	<link rel="stylesheet" href="css/fonts/entypo/css/entypo.css">
+	<!-- <link rel="stylesheet" href="vendor/owl-carousel/owl.carousel.css" media="screen">
+	<link rel="stylesheet" href="vendor/owl-carousel/owl.theme.css" media="screen"> -->
+	<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.css" media="screen">
+	<link rel="stylesheet" href="vendor/flexslider/flexslider.css" media="screen">
+	<link rel="stylesheet" href="vendor/job-manager/frontend.css" media="screen">
+
+	<!-- Theme CSS-->
+	<link rel="stylesheet" href="css/theme.css">
+	<link rel="stylesheet" href="css/theme-elements.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    
+    <!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicons/favicon.ico">
+	<link rel="apple-touch-icon" sizes="120x120" href="images/favicons/favicon-120.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/favicons/favicon-152.png">
+    
     <title>Boardman - {{ $title }}</title>
 </head>
 <body>
